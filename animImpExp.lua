81d8c691a7dfd174055fac0f9528e403142c83e2
jaytc-design <66380812+jaytc-design@users.noreply.github.com>
2021-03-11 12:02:55 -0500

    Adding additional bug fixes and usage comment

--- animImpExp.lua
+++ animImpExp.lua
@@ -20,19 +20,40 @@
 			
 			The import function returns an array, the index is the frame number, based off of render frames.
 			In the array, each value is a dictionary, the indexes are the string names of bodyparts and the value is a CFrame.
+			
+	Example On How You Might Move Character Parts:
+	
+	local i = 0
+	local val = import()
+	local connection
+	connection = game:GetService("RunService").RenderStepped:Connect(function() -- We use render stepped because the animation is recorded using RenderStepped, so it is best to run it the same way
+		i = i+1
+		if val[i] then
+			for i,v in pairs(val[i]) do
+				game.Players.LocalPlayer.Character:FindFirstChild(i).CFrame = v
+			end
+		else
+			connection:Disconnect()
+		end
+	end)
+	
 ]]--
 
 local recordTime = 10 -- How long in seconds to record you animation
-local filename = "default.txt" -- The name of the file to write the data to and retrieve from
+local f = "hmm.txt" -- The name of the file to write the data to and retrieve from
 
 -- DONT TOUCH ANYTHING BELOW HERE --	
 local function export()
 local anims = {}
 local a = tick()
 local conn
-local wai = true
 conn = game:GetService("RunService").RenderStepped:Connect(function()
-	if tick()-a >= recordTime then conn:Disconnect();wai = false; return end
+	print('x')
+	if tick()-a >= recordTime then 
+		conn:Disconnect()
+		writefile(f,game:GetService("HttpService"):JSONEncode(anims))
+		print('Export Complete...')
+	return end
 	anims[#anims+1] = {}
 	for i,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
 		if v:IsA("BasePart") then
@@ -40,13 +61,10 @@ conn = game:GetService("RunService").RenderStepped:Connect(function()
 		end
 	end
 end)
-repeat wait() until wai == false
-	writefile(filename,game:GetService("HttpService"):JSONEncode(anims))
-	print('Import Complete...')
 end
 
 local function import()
-	local txt = readfile(filename) or "[{}]"
+	local txt = readfile(f) or "[{}]"
 	local parsed = game:GetService("HttpService"):JSONDecode(txt)
 	for i1,v1 in pairs(parsed) do
 		for i2,v2 in pairs(v1) do
@@ -57,6 +75,6 @@ local function import()
 			parsed[i1][i2] = CFrame.new(table.unpack(split))
 		end
 	end
-	print('Export Complete...')
+	print('Import Complete...')
 	return parsed
 end
